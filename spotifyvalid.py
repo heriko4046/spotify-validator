57f92bff15dcd5b435b3848eb36e9adc845ce06d
heriko4046 <118543625+heriko4046@users.noreply.github.com>
2024-01-30 01:17:50 +0700

    Add files via upload

---
+++ spotifyvalid.py
@@ -0,0 +1,56 @@
+import requests as r
+import os
+from fake_useragent import UserAgent
+from colorama import Fore, Style
+
+class Spotify:
+    def __init__(self):
+        self.api = r.Session()
+        self.ua = UserAgent().random  # Fix: Use `random` as a method
+        self.green = Fore.GREEN
+        self.reset = Style.RESET_ALL
+        self.red = Fore.RED
+
+    def valid(self, email):
+        # Post
+        headers = {
+            'User-Agent': self.ua,
+        }
+
+        data = {
+            'validate': '1',
+            'email': email
+        }
+
+        try:
+            resp = self.api.get('https://spclient.wg.spotify.com/signup/public/v1/account', headers=headers, params=data)
+
+            # Json 
+            data = resp.json()
+            country = data['country']
+
+            # Validator
+            if 'is already registered to an account' in resp.text:
+                print(f'[> Email: {email} | {self.green}Valid{self.reset}')        
+            else:
+                print(f'[> Email: {email} | {self.red}Invalid{self.reset}')
+
+            with open('spotifyvalid.txt', 'a') as file:
+                file.write(f'[> Email: {email} | Valid | {country}' + '\n')
+        except KeyboardInterrupt:
+            print('[!] Process Interrupted!')
+        except Exception as e:
+            print(f'[!] Error: {e}')
+
+if __name__ == "__main__":
+    try:
+        bot = Spotify()
+        os.system('cls')
+        print('<[ Spotify Email Validator ]>')
+        email = input('[> File Path: ')  
+        with open(email, 'r') as file:  
+            for email in file:
+                bot.valid(email.strip())
+        print('\n[!] Result Saved: spotifyvalid.txt') 
+    except KeyboardInterrupt:
+        print('[!] Process Interrupted!')
